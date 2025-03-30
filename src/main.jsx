d1a3333216f1d9c7065db413458ecac105cc3b67
iosonoandres <massimoandres20@gmail.com>
2025-03-30 16:20:11 +0200

    a

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('✅ main.jsx è stato eseguito');
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
